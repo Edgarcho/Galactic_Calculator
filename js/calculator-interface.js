47eaf786eb04fb792997f73f4bf41570628b1e20
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-18 22:06:49 -0800

    add submit function to interface

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -1,1 +1,11 @@
 import { Calculator } from './../js/calculator.js';
+
+$(document).ready(function() {
+  $("#yearForm").submit(function(event) {
+    event.preventDefault();
+    let userAge = parseInt($("#age").val());
+    let userlifeExp = parseInt($("#lifeExp").val());
+    let userResult = new Calculator(userAge, userlifeExp);
+    $("#result").append("<ul><li></li></ul>");
+  })
+})
