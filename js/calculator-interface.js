fd2f7a1a8fbb8b3e4af1eea955bda154e71908e6
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-19 09:41:42 -0800

    Add for loop in interface

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -3,9 +3,15 @@ import { Calculator } from './../js/calculator.js';
 $(document).ready(function() {
   $("#yearForm").submit(function(event) {
     event.preventDefault();
+    const planetsName = ["Mercury","Venus","Mars","Jupiter"];
     let userAge = parseInt($("#age").val());
     let userlifeExp = parseInt($("#lifeExp").val());
-    let userResult = new Calculator(userAge, userlifeExp);
-    $("#result").append("<ul><li></li></ul>");
-  })
-})
+    let userResult = new Calculator(userAge);
+    let finalResult= userResult.convert();
+    console.log(finalResult);
+    console.log(planetsName);
+    for(let i = 0; i < finalResult.length; i+=1){
+      $('#result').append("<tr><td>finalResult[i]<td></tr>");
+    }
+  });
+});
