a73f0c430142097e86ba424528e3bee794c639c5
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-19 11:17:12 -0800

    Create checkLifeExp functio in calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,36 +1,28 @@
 export class Calculator {
-  constructor(age) {
+  const planetsYear = [0.241, 0.615, 1.88, 11.86];
+  let humanYearForPlanets = [];
+  let lifeExpectancy = [];
+
+  constructor(age, lifeExp) {
     this.age = age;
+    this.lifeExp = lifeExp;
   }
 
   convert(){
-    const planetsYear = [0.241, 0.615, 1.88, 11.86];
-    let humanYearForPlanets = [];
     for (let i = 0; i < planetsYear.length; i += 1){
        let year = this.age/planetsYear[i];
        humanYearForPlanets.push(Math.ceil(year));
     }
     return humanYearForPlanets;
   }
+  checkLifeExp(){
+    for (let i = 0; i < humanYearForPlanets.length; i += 1){
+      if (humanYearForPlanets[i] === 79 ) || (humanYearForPlanets[i] === 86 ) || (humanYearForPlanets[i] === 82 ) || ((humanYearForPlanets[i] === 78 )) {
+        lifeExpectancy.push("Surpassed life expectancy");
+      }else {
+        lifeExpectancy.push("Still below life expectancy");
+      }
+    }
+    return lifeExpectancy;
+  }
 }
-
-
-/*
-//planet orbit for one Earth year
-let mercury = 0.241
-let venus = 0.615
-let mars = 1.881
-let jupiter = 11.86
-let saturn = 29.46
-let uranus = 84.01
-let neptune = 164.8
-let pluto = 248.6
-//age/planet year = age on planet
-//life expectancy for 2018
-let usa = 79
-let japan = 86
-let france = 82
-let mexico = 78
-let spain = 82
-let canada = 82
-*/
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -2,15 +2,16 @@ import { Calculator } from './../js/calculator.js';
 
 describe('Calculator', function(){
   it('should test whether calculator has created new calculator instance', function(){
-    let userAge = new Calculator(25);
+    let userAge = new Calculator(25, "United States");
     expect(userAge.age).toEqual(25);
+    expect(userAge.lifeExp).toEqual("United States");
   });
   it('should test whether calculator has converted age into Mercury, Venus, Mars and Jupiter years', function(){
-    let userAge = new Calculator(25);
+    let userAge = new Calculator(25, "United States");
     expect(userAge.convert()).toEqual([104,41,14,3]);
   });
   it('should test whether calculator has check if you have surpassed the average life expectancy on each planet', function(){
-    let userAge = new Calculator(25);
-    expect(userAge.expectancy()).toEqual([]);
+    let userAge = new Calculator(25, "United States");
+    expect(userAge.convert().checkLifeExp()).toEqual(["Surpassed life expectancy","Still below life expectancy","Still below life expectancy","Still below life expectancy"]);
   })
 });
