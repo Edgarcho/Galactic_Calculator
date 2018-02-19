0f4b469c326fe204f9121e55f2411e47943d8233
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-19 10:50:49 -0800

    Refactor index and interface files

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -3,15 +3,18 @@ import { Calculator } from './../js/calculator.js';
 $(document).ready(function() {
   $("#yearForm").submit(function(event) {
     event.preventDefault();
-    const planetsName = ["Mercury","Venus","Mars","Jupiter"];
     let userAge = parseInt($("#age").val());
-    let userlifeExp = parseInt($("#lifeExp").val());
-    let userResult = new Calculator(userAge);
+    let userlifeExp = $("#lifeExp").val();
+    let userResult = new Calculator(userAge, userlifeExp);
     let finalResult= userResult.convert();
-    console.log(finalResult);
-    console.log(planetsName);
-    for(let i = 0; i < finalResult.length; i+=1){
-      $('#result').append("<tr><td>finalResult[i]<td></tr>");
-    }
+    let lifeExpResult= userResult.checkLifeExp();
+    $("#result .planet1").text(finalResult[0]);
+    $("#result .lifeExp1").text(lifeExpResult[0]);
+    $("#result .planet2").text(finalResult[1]);
+    $("#result .lifeExp2").text(lifeExpResult[1]);
+    $("#result .planet3").text(finalResult[2]);
+    $("#result .lifeExp3").text(lifeExpResult[2]);
+    $("#result .planet4").text(finalResult[3]);
+    $("#result .lifeExp4").text(lifeExpResult[3]);
   });
 });
